07a5022476b7be69181cd726619ec3e44e6ba932
Mantas Klimašauskas <mantasklimasauskas@gmail.com>
2019-11-11 18:46:20 +0200

    Added ParsFood-Commons repo to the project, enabled IdPlugin for Vue

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
+import "reflect-metadata";
+
 import Vue from "vue"
 import App from "@/App.vue"
 import router from "@/router"
 import {store} from "@/store/store"
 import "@/firebase";
+import IdPlugin from "-/plugins/IdPlugin/IdPlugin";
 
 Vue.config.productionTip = false;
+
+Vue.use(IdPlugin);
 
 new Vue({
     router,
