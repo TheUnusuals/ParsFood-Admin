4da58a59bc4cd2dfb925f4c6846c162fba889d2c
Mantas Klimašauskas <mantasklimasauskas@gmail.com>
2019-11-12 22:48:41 +0200

    Fixed ParsFood-Common integration, updated packages
    ParsFood-Common integration now uses a symlink to just add the common files right into the source code, simplifying everything.

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,14 @@ import App from "@/App.vue"
 import router from "@/router"
 import {store} from "@/store/store"
 import "@/firebase";
-import IdPlugin from "-/plugins/IdPlugin/IdPlugin";
+import IdPlugin from "@/common/plugins/vue/IdPlugin";
 
 Vue.config.productionTip = false;
 
 Vue.use(IdPlugin);
 
 new Vue({
-    router,
-    store,
+    router: router,
+    store: store,
     render: h => h(App)
 }).$mount("#app");
