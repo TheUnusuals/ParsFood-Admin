26d61f632575b60a965804bead44f3e88178dbf5
Mantas Klimašauskas <mantasklimasauskas@gmail.com>
2020-01-01 19:22:29 +0200

    #8, #31 Added AdditionalIngredientsGroupComponent.vue for viewing, editing and creating additional ingredients groups

--- src/plugins/validation.ts
+++ src/plugins/validation.ts
@@ -1,5 +1,5 @@
 import {extend, localize} from "vee-validate";
-import {email, required} from "vee-validate/dist/rules";
+import {email, min_value, required} from "vee-validate/dist/rules";
 import {i18n} from "@/plugins/i18n";
 
 import en from "vee-validate/dist/locale/en.json";
@@ -7,6 +7,7 @@ import lt from "vee-validate/dist/locale/lt.json";
 
 extend("required", required);
 extend("email", email);
+extend("min_value", min_value);
 
 localize({en, lt});
 localize(i18n.locale);
