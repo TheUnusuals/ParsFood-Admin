c97d7ab47fc0e39cb5639932eb982c0ca8b09e1b
Mantas Klimašauskas <mantasklimasauskas@gmail.com>
2020-01-07 20:42:43 +0200

    #4, #36 Added MenuItemComponent.vue

--- src/plugins/firebase.ts
+++ src/plugins/firebase.ts
@@ -2,12 +2,16 @@ import * as firebase from "firebase/app";
 import "firebase/functions";
 import "firebase/auth";
 import "firebase/firestore";
+import "firebase/storage";
 import config from "@/firebase.config";
 import {setFirestore} from "@/common/js/firestore-utils";
+import {setStorage} from "@/common/js/storage-utils";
 
 export const firebaseApp = firebase.initializeApp(config);
 export const firebaseFunctions = firebaseApp.functions("europe-west1");
 export const firebaseAuth = firebaseApp.auth();
 export const firebaseFirestore = firebaseApp.firestore();
+export const firebaseStorage = firebaseApp.storage();
 
 setFirestore(firebaseFirestore);
+setStorage(firebaseStorage);
--- src/plugins/validation.ts
+++ src/plugins/validation.ts
@@ -1,13 +1,21 @@
-import {extend, localize} from "vee-validate";
-import {email, min_value, required} from "vee-validate/dist/rules";
+import {configure, extend} from "vee-validate";
+import {email, image, min_value, required} from "vee-validate/dist/rules";
 import {i18n} from "@/plugins/i18n";
-
-import en from "vee-validate/dist/locale/en.json";
-import lt from "vee-validate/dist/locale/lt.json";
 
 extend("required", required);
 extend("email", email);
 extend("min_value", min_value);
+extend("image", image);
 
-localize({en, lt});
-localize(i18n.locale);
+extend("min_length", {
+    validate(value, {length}) {
+        return value.length >= length
+    },
+    params: ["length"]
+});
+
+configure({
+    defaultMessage: (field, values: any) => {
+        return i18n.t(`$vee-validate.${values._rule_}`, values) as string;
+    }
+});
